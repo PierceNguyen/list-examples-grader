f6b68675b0bea6710206b4489cafb2a97f8d8381
PierceNguyen <78495327+PierceNguyen@users.noreply.github.com>
2024-05-28 11:42:16 -0700

    bash changes

--- grade.sh
+++ grade.sh
@@ -11,6 +11,26 @@ echo 'Finished cloning'
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+#check if java file exists
+if ! test -f student-submission/ListExamples.java; then
+  echo "File does not exist."
+  exit
+fi
+
+#copy files to grading-area
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+#cd grading-area
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/*.java
+if [ $? -ne 0 ]; then
+    echo "Your submission didn't compile"
+    exit
+fi
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore grading-area/TestListExamples > grading-area/JUnit-output.txt
+
+#figure out grade part
+TestsRun=`grep -o "Tests run:.." grading-area/JUnit-output.txt`
+TestsFailed=`grep -o "Failures:.." grading-area/JUnit-output.txt`
