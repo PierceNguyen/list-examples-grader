a5538ac1ae25cffbf8e2fa40523716c5af2a5407
Klein-Shen <165833763+Klein-Shen@users.noreply.github.com>
2024-05-28 12:39:59 -0700

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,21 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testMergeLeftEnd() {
+    List<String> right = Arrays.asList("a", "b", "c", "c", "d", "e", "y", "z");
+    List<String> left = Arrays.asList("a", "d", "x", "z");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "c", "d", "d", "e", "x", "y", "z", "z");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter() {
+    List<String> list = Arrays.asList("a", "b", "c", "moon", "1", "c", "a", "d", "m", "moon");
+    IsMoon sc = new IsMoon();
+    List<String> expected = Arrays.asList("moon", "moon");
+    assertEquals(expected, ListExamples.filter(list, sc));
+  }
 }
